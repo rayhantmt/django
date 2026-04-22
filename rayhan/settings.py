b9f12e2d8adbca4d39da303a10e7c84f2b59d84d
rayhantmt <rayhantmt@gmail.com>
2026-04-23 01:25:45 +0600

    crated the app api

--- rayhan/settings.py
+++ rayhan/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    #'api', # We will create this app next
 ]
 
 MIDDLEWARE = [
